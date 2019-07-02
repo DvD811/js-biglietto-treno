45bc50a37a8c501301aff11b8bb3d0e7a277e8c2
DvD811 <davide.gallizioli@gmail.com>
2019-07-02 09:56:59 +0200

    up

--- script.js
+++ script.js
@@ -7,18 +7,21 @@ var yeage;
 var sconto;
 
 var age = (2019 - yeage);
-
+var bigliettoInt = (0.21 * lungh);
 
 if (age < 18) {
-    sconto = (lungh / 100) * 20;
+    sconto = 20;
+    var biglietto = (bigliettoInt / 100) * 20;
 }
 if (age > 65) {
-    sconto = (lungh / 100) * 40;
+    sconto = 40;
+    var biglietto = (bigliettoInt / 100) * 40;
 } else {
     sconto = 0;
+    var biglietto = (bigliettoInt);
 };
 console.log(sconto)
-var biglietto = ((0.21 * lungh) - sconto);
+
 console.log(biglietto);
-document.getElementById("prezzoTreno").innerHTML = "Il prezzo del tuo biglietto sarà:" + biglietto + ". Ti è stato applicato uno sconto del:" + sconto;
+document.getElementById("prezzoTreno").innerHTML = "Il prezzo del tuo biglietto sarà:" + biglietto + ". Ti è stato applicato uno sconto del:" + sconto + "%.";
 //  0.21 cent x km ma con uno sconto di 20% if < 18 anni e del 40% if> 65 anni
