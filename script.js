de634cbb348b23ab0a15cbc9723528f8e3faafc1
DvD811 <davide.gallizioli@gmail.com>
2019-07-02 09:25:20 +0200

    upd

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 // chiedere all' utente l' età
-var age = prompt("Inserisci il tuo anno di nascita");
+var yeage = prompt("Inserisci il tuo anno di nascita");
 // chiedere all' utente i chilometri del viaggio
 var lungh = prompt(" Inserisci il chilometraggio del tuo viaggio");
 //  calcolo del prezzo del biglietto 
-var age;
+var yeage;
 var sconto;
 
-if ((2019 - age) < 18) {
-    sconto = 20 %
+var age = (2019 - yeage);
+
+
+if (age < 18) {
+    sconto = (lungh / 100) * 20;
 }
-if ((2019 - age) > 65) {
-    sconto = 40 % ;
+if (age > 65) {
+    sconto = (lungh / 100) * 40;
 } else {
-    sconto = 0 % ;
+    sconto = 0;
 };
-console.log(sconto);
+console.log(sconto)
+var biglietto = ((0.21 * lungh) - sconto);
+console.log(biglietto);
+document.getElementById("prezzoTreno").innerHTML = "Il prezzo del tuo biglietto sarà:" + biglietto + ". Ti è stato applicato uno sconto del:" + sconto;
 //  0.21 cent x km ma con uno sconto di 20% if < 18 anni e del 40% if> 65 anni
